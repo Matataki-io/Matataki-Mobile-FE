0abdde9b35449e5ffdfbe9d890646dacc1492c89
zxplus <shellteo@163.com>
2019-08-23 17:51:13 +0800

    🐛 bugfix:fixed github login

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -10,14 +10,14 @@ export default {
   name: 'LoginPage',
   computed: {},
   created() {
-    const isWeixin = () => /micromessenger/.test(navigator.userAgent.toLowerCase())
+    // const isWeixin = () => /micromessenger/.test(navigator.userAgent.toLowerCase())
     const { protocol, host } = window.location
     const { path, query } = this.$route
     const { code, from } = query
     const clientID = process.env.VUE_APP_GITHUB_CLIENT_ID
     const scope = 'read:public_repo,read:user'
     // 判断是否来自微信
-    if (isWeixin) from = 'wx'
+    // if (isWeixin) from = 'wx'
     const redirectUri = `${protocol}//${host}${path}?from=${from}` // 範例值
     if (!code) {
       // 跳轉
@@ -26,9 +26,9 @@ export default {
       // 如果是pc的话，需要跳转到pc端
       if (from === 'pc') {
         window.location = `${process.env.VUE_APP_PC_URL}/login?code=${code}&from=${from}`
-      } else if (from === 'wx' && `${protocol}//${host}`!== process.env.VUE_APP_WX_URL) {
+      // } else if (from === 'wx' && `${protocol}//${host}`!== process.env.VUE_APP_WX_URL) {
         // 如果是微信，且url不等于wx，就需要跳转
-        window.location = `${process.env.VUE_APP_WX_URL}/login?code=${code}&from=${from}`
+        // window.location = `${process.env.VUE_APP_WX_URL}/login?code=${code}&from=${from}`
       } else {
         this.signIn({ code, idProvider: 'GitHub' })
           .then(() => {
