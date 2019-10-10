fa1a3e72f321ac6fd5f767fd93ba290724ed307d
xiaotiandada <952822399@qq.com>
2019-10-10 14:14:38 +0800

    修改GitHub登录代码

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -14,31 +14,46 @@ export default {
     const { protocol, host } = window.location
     const { path, query } = this.$route
     const { code, from } = query
+    const APP_URL = process.env.VUE_APP_URL
+
     const clientID = process.env.VUE_APP_GITHUB_CLIENT_ID
     const scope = 'read:public_repo,read:user'
     // 判断是否来自微信
     // if (isWeixin) from = 'wx'
-    const redirectUri = `${protocol}//${host}${path}?from=${from}` // 範例值
+    const redirectUri = `${APP_URL}/login/github` // 範例值
+
+    // const redirectUri = `${protocol}//${host}${path}?from=${from}` // 範例值
+    // http://localhost:8080/login/github?from=/
+    alert(redirectUri)
     if (!code) {
       // 跳轉
       window.location = `https://github.com/login/oauth/authorize?client_id=${clientID}&redirect_uri=${redirectUri}&scope=${scope}`
     } else {
       // 如果是pc的话，需要跳转到pc端
-      if (from === 'pc') {
-        window.location = `${process.env.VUE_APP_PC_URL}/login?code=${code}&from=${from}`
-        // } else if (from === 'wx' && `${protocol}//${host}`!== process.env.VUE_APP_WX_URL) {
-        // 如果是微信，且url不等于wx，就需要跳转
-        // window.location = `${process.env.VUE_APP_WX_URL}/login?code=${code}&from=${from}`
-      } else {
-        this.signIn({ code, idProvider: 'GitHub' })
-          .then(() => {
-            this.$backendAPI.accessToken = this.currentUserInfo.accessToken
-          })
-          .catch(() => {})
-          .then(() => {
-            this.$router.push(from)
-          })
-      }
+      // if (from === 'pc') {
+      //   window.location = `${process.env.VUE_APP_PC_URL}/login?code=${code}&from=${from}`
+      //   // } else if (from === 'wx' && `${protocol}//${host}`!== process.env.VUE_APP_WX_URL) {
+      //   // 如果是微信，且url不等于wx，就需要跳转
+      //   // window.location = `${process.env.VUE_APP_WX_URL}/login?code=${code}&from=${from}`
+      // } else {
+      //   this.signIn({ code, idProvider: 'GitHub' })
+      //     .then(() => {
+      //       this.$backendAPI.accessToken = this.currentUserInfo.accessToken
+      //     })
+      //     .catch(() => {})
+      //     .then(() => {
+      //       this.$router.push(from)
+      //     })
+      // }
+
+      this.signIn({ code, idProvider: 'GitHub' })
+        .then(() => {
+          this.$backendAPI.accessToken = this.currentUserInfo.accessToken
+        })
+        .catch(() => {})
+        .then(() => {
+          this.$router.push(from)
+        })
     }
   },
   methods: {
