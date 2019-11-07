e73233e4aede5fee4b9f449e4916135f7e68d1d0
shellteo <shellteo@163.com>
2019-11-07 16:45:52 +0800

    添加交易成功提示

--- src/views/exchange/components/OrderModal.vue
+++ src/views/exchange/components/OrderModal.vue
@@ -83,7 +83,7 @@
       
       <template v-if="needPay > 0">
         <div v-if="isWeixin" class="wxpay-btn">
-          <el-button type="success" @click="loginAndPay">
+          <el-button type="primary" @click="loginAndPay">
             使用微信支付
             <svg-icon icon-class="wxpay" class="wxpay-icon"/>
           </el-button>
@@ -274,8 +274,10 @@ export default {
             if(res.err_msg == "get_brand_wcpay_request:ok" ){
               // 使用以上方式判断前端返回,微信团队郑重提示：
               //res.err_msg将在用户支付成功后返回ok，但并不保证它绝对可靠。
-              console.log('支付成功')
-              alert('支付成功')
+              this.successNotice("交易成功，即将刷新页面");
+              setTimeout(() => {
+                window.location.reload();
+              }, 2000);
             } 
         }); 
       }
