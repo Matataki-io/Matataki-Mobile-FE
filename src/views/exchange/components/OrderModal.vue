f30dd1f309505f74652332bf14a1ca6d672702dd
shellteo <shellteo@163.com>
2019-11-07 17:02:04 +0800

    fixed bug!

--- src/views/exchange/components/OrderModal.vue
+++ src/views/exchange/components/OrderModal.vue
@@ -260,6 +260,7 @@ export default {
     },
     weakWeixinPay(order) {
       const { appId, timeStamp, nonceStr, signType, paySign } = order
+      const self = this
       function onBridgeReady(){
         WeixinJSBridge.invoke(
             'getBrandWCPayRequest', {
@@ -274,8 +275,7 @@ export default {
             if(res.err_msg == "get_brand_wcpay_request:ok" ){
               // 使用以上方式判断前端返回,微信团队郑重提示：
               //res.err_msg将在用户支付成功后返回ok，但并不保证它绝对可靠。
-              alert('交易成功，即将刷新')
-              this.successNotice("交易成功，即将刷新页面");
+              self.successNotice("交易成功，即将刷新页面");
               setTimeout(() => {
                 window.location.reload();
               }, 2000);
