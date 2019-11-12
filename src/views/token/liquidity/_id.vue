e8128efd0acb65c0a487f6341be0c88b24480c52
神樹桜乃 <kodama@sakuno.moe>
2019-11-12 18:57:53 +0800

    Add pages about token liquidity

---
+++ src/views/token/liquidity/_id.vue
@@ -0,0 +1,53 @@
+<template>
+  <layout>
+    <BasePull
+      :url-replace="$route.params.id + ''"
+      :params="pull.params"
+      :api-url="pull.apiUrl"
+      :loading-text="$t('not')"
+      :is-obj="{ type: 'newObject', key: 'data', keys: 'list' }"
+      :need-access-token="true"
+      :auto-request-time="0"
+      @getListData="getListData"
+    >
+      <card v-for="item in pull.list" :key="item.id" :card="item" :decimals="4"></card>
+    </BasePull>
+  </layout>
+</template>
+
+<script>
+import card from '@/components/minetoken_liquidity_card/index.vue'
+import layout from '@/components/token/token_layout.vue'
+
+export default {
+  components: {
+    card,
+    layout
+  },
+  data() {
+    return {
+      pull: {
+        params: {
+          pagesize: 20
+        },
+        apiUrl: 'liquidityUserList',
+        list: []
+      },
+      currentPage: Number(this.$route.query.page) || 1,
+      loading: false, // 加载数据
+      total: 0
+    }
+  },
+  methods: {
+    getListData(res) {
+      this.pull.list = res.list
+    }
+  }
+}
+</script>
+
+<style scoped>
+.pagination {
+  margin-top: 40px;
+}
+</style>
