bce98bf326d69742bedabd1cd213feb37750da5f
韦海晖 <franciswei98@gmail.com>
2019-11-18 15:55:53 +0800

    fix: handle Autoprefixer warning

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,9 @@
 module.exports = {
   plugins: {
-    autoprefixer: {
-      browsers: ['Android >= 4.0', 'iOS >= 7']
-    },
+    // 应该该用 browserlistrc 了
+    // autoprefixer: {
+    //   browsers: ['Android >= 4.0', 'iOS >= 7']
+    // },
     'postcss-pxtorem': {
       rootValue: 16,
       propList: ['*']
