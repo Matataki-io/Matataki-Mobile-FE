fd499029174e2901a6def6c08e5add4dcd309fc1
xiaotian <952822399@qq.com>
2019-11-26 16:17:13 +0800

    update 打包配置

--- vue.config.js
+++ vue.config.js
@@ -19,6 +19,7 @@ function resolve(dir) {
 const isDev = process.env.NODE_ENV === 'development'
 
 module.exports = {
+  assetsPublicPath: '/',
   chainWebpack: config => {
     // 移除 prefetch 插件
     config.plugins.delete('prefetch')
