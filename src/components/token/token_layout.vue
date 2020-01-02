eea6ce71319c4b4f0846cff6ddcdae6956364e81
LittleSound <464388324@qq.com>
2020-01-02 20:20:01 +0800

    对接Fan群API

--- src/components/token/token_layout.vue
+++ src/components/token/token_layout.vue
@@ -141,7 +141,7 @@
 
     <tokenBuyCard :token="minetokenToken" />
 
-    <TokenJoinFandom :token-symbol="minetokenToken.symbol || ''" />
+    <TokenJoinFandom :token-symbol="minetokenToken.symbol || ''" :token-id="Number($route.params.id)" />
 
     <div class="about">
       <h2 class="token-title">
