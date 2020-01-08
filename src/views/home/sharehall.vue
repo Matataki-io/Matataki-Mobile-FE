fcdd934b900b1a219b18f94e46fa30bbef598918
xiaotian <952822399@qq.com>
2020-01-08 20:39:53 +0800

    分享大厅样式修改

--- src/views/home/sharehall.vue
+++ src/views/home/sharehall.vue
@@ -368,9 +368,10 @@ export default {
   margin: 0 0 4px;
   padding: 10px 20px;
   position: sticky;
-  top: 46px;
+  top: 45px;
   background: #fff;
   z-index: 9;
+  box-sizing: border-box;
   &.active {
     border-bottom: 0.0625rem solid #eaeaea;
     box-shadow: 0 0 10px 4px rgba(0,0,0,.08);
