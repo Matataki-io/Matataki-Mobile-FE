e5d519fe257765ba3a65ddce01138782696b31a3
韦海晖 <franciswei98@gmail.com>
2020-01-09 16:42:01 +0800

    Revert "feat: broadcast channel"
    This reverts commit f59e7145057ae53f4b5cc78343c2a543c815cc54.

--- src/main.js
+++ src/main.js
@@ -18,7 +18,6 @@ import './registerServiceWorker'
 // import "lib-flexible"; // 适配
 
 // Vue plugins
-import './plugins'
 import './plugins/iview'
 import './plugins/vant'
 import './plugins/element-ui'
